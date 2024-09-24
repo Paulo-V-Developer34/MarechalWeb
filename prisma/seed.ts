9cfabe5c369fd6e6762dd9df2b26b414fa30d19e
Paulo-V-Developer34 <paulov2007nv@gmail.com>
2024-09-24 12:29:58 -0300

    add noticia bd connection

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,88 +6,113 @@ import { Prisma, PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const initialUsers: Prisma.UserCreateInput[] = [
-    {
-        nome: "Paulo Victor Nunes Cunha",
-        sala: "3NT",
-        senha: "Senhasegura",
-    },
-    {
-        nome: "Ian Peixinho Barbosa",
-        sala: "3NT",
-    },
-    {
-        nome: "Isac Monteiro Barbosa",
-        sala: "2A",
-    },
-    {
-        nome: "Diretor da Escola",
-        senha: "Senhasupersegura",
-        tipo: 3
-    },
-    {
-        nome: "Aluno ajudante da escola",
-        sala: "1C",
-        tipo: 2
-    },
+  {
+    nome: 'Paulo Victor Nunes Cunha',
+    sala: '3NT',
+    senha: 'Senhasegura',
+  },
+  {
+    nome: 'Ian Peixinho Barbosa',
+    sala: '3NT',
+  },
+  {
+    nome: 'Isac Monteiro Barbosa',
+    sala: '2A',
+  },
+  {
+    nome: 'Diretor da Escola',
+    senha: 'Senhasupersegura',
+    tipo: 3,
+  },
+  {
+    nome: 'Aluno ajudante da escola',
+    sala: '1C',
+    tipo: 2,
+  },
 ]
 
 const initialNoticias: Prisma.NoticiaCreateInput[] = [
-    {
-        title: "Notícia muito interessante",
-        slug: "noticia-muito-interessante",
-        intro: "Essa notícia é a mais incrível de todas as notícias que você já viu na vida",
-        content: "Essa notícia conta a história de como a notícia mais incrível do mundo foi criada, ela é realmente muito incrível",
-        Autor: {
-            connectOrCreate: {
-                where: {
-                    nome: "Diretor da Escola"
-                },
-                create: {
-                    nome: "Vice diretor da escola"
-                }
-            }
-        }
+  {
+    title: 'Notícia muito interessante',
+    slug: 'noticia-muito-interessante',
+    intro:
+      'Essa notícia é a mais incrível de todas as notícias que você já viu na vida',
+    content:
+      'Essa notícia conta a história de como a notícia mais incrível do mundo foi criada, ela é realmente muito incrível',
+    nomeautor: 'Diretor da Escola',
+    Autor: {
+      connectOrCreate: {
+        where: {
+          nome: 'Diretor da Escola',
+        },
+        create: {
+          nome: 'Vice diretor da escola',
+        },
+      },
     },
-    {
-        title: "Notícia muito interessante pt2",
-        slug: "noticia-muito-interessante-pt2",
-        intro: "Essa notícia é a mais incrível de todas as notícias que você já viu na vida",
-        content: "Essa notícia conta a história de como a notícia mais incrível do mundo foi criada, ela é realmente muito incrível",
-        Autor: {
-            connectOrCreate: {
-                where: {
-                    nome: "Diretor da Escola"
-                },
-                create: {
-                    nome: "Vice diretor da escola"
-                }
-            }
-        }
+  },
+  {
+    title: 'Notícia muito interessante pt2',
+    slug: 'noticia-muito-interessante-pt2',
+    intro:
+      'Essa notícia é a mais incrível de todas as notícias que você já viu na vida',
+    content:
+      'Essa notícia conta a história de como a notícia mais incrível do mundo foi criada, ela é realmente muito incrível',
+    nomeautor: 'Diretor da Escola',
+    Autor: {
+      connectOrCreate: {
+        where: {
+          nome: 'Diretor da Escola',
+        },
+        create: {
+          nome: 'Vice diretor da escola',
+        },
+      },
     },
+  },
+  {
+    title: 'Notícia muito interessante pt3',
+    slug: 'noticia-muito-interessante-pt3',
+    intro:
+      'Essa notícia é a mais incrível de todas as notícias que você já viu na vida',
+    content:
+      'Essa notícia conta a história de como a notícia mais incrível do mundo foi criada, ela é realmente muito incrível',
+    nomeautor: 'Diretor da Escola',
+    Autor: {
+      connectOrCreate: {
+        where: {
+          nome: 'Diretor da Escola',
+        },
+        create: {
+          nome: 'Vice diretor da escola',
+        },
+      },
+    },
+  },
 ]
 
 async function main() {
-  console.log("Começando a semear dados...")
-  for (const user of initialUsers) {
-    const newuser = await prisma.user.create({
-        data: user,
-    })
-    console.log(`user com o Nome ${newuser.nome} foi inserido`)
-  }
+  console.log('Começando a semear dados...')
+  //   for (const user of initialUsers) {
+  //     const newuser = await prisma.user.create({
+  //       data: user,
+  //     })
+  //     console.log(`user com o Nome ${newuser.nome} foi inserido`)
+  //   }
   for (const noticia of initialNoticias) {
     const newnoticia = await prisma.noticia.create({
-        data: noticia,
+      data: noticia,
     })
     console.log(`noticia com o título ${newnoticia.title} foi inserido`)
   }
-  console.log("Semeação concluída")
+  console.log('Semeação concluída')
 }
 
 main()
   .then(async () => {
     await prisma.$disconnect()
   })
-  .catch(async (e) => {
+  .catch(async e => {
     console.error(e)
     await prisma.$disconnect()
     process.exit(1)
