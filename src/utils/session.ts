58941f06c5c874d0b7b419b7504c0b6410b6ff20
Paulo-V-Developer34 <165535750+Paulo-V-Developer34@users.noreply.github.com>
2025-03-04 18:15:40 -0300

    fix get url error

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -8,7 +8,7 @@ import { cookies } from 'next/headers'
 import { type NextRequest, NextResponse } from 'next/server'
 
 //interfaces
-export interface user {
+export type user = {
   nome: string
   sala: string | null
   tipo: number //depois eu irei mudar para string
